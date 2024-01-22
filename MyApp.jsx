b477a96b22ad2c6fc4ecfcee54a42e32a934c308
jorgitom1980 <141953007+jorgitom1980@users.noreply.github.com>
2024-01-22 18:54:46 -0500

    Add files via upload

---
+++ MyApp.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Profile from './Profile';
+
+const MyApp = () => {
+  return (
+    <div>
+      <h1>Welcome to My App!</h1>
+      <Profile />
+    </div>
+  );
+};
+
+export default MyApp;
+
---
+++ Profile.jsx
@@ -0,0 +1,21 @@
+// Profile.jsx
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <section>
+      <h2>Jorge Moreno</h2>
+      <img src='https://media.licdn.com/dms/image/D4E03AQE6ihI9cBRQ0Q/profile-displayphoto-shrink_800_800/0/1693768120623?e=2147483647&v=beta&t=Cy7jctYNv9PqOwfJey32jYDGWGbtdokyuHXocLlXUbY' alt='Your Profile' />
+  
+      <address>
+        <a href='mailto:jorgitom1980@gmail.com'>📧 jorgitom1980@gmail.com</a>
+        <br />
+        <a href='tel:+813.263.4497'>📞(813)263-4497</a>
+        <br />  
+                
+      </address>
+    </section>
+  );
+};
+
+export default Profile;
